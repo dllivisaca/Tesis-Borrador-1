eb4ac8a68d4380311ea36889985629fd9757a56d
Daisy Llivisaca <daisynohelia@gmail.com>
2024-08-23 15:57:22 -0500

    login admin con menu para gestionar horarios disponibles (agregar, eliminar, ver) queda pendiente opción de agendar, menú de citas agendadas (ver y cancelar). login paciente con menu de citas agendadas (ver y cancelar) y menu de horarios disponibles (ver y agendar).

--- admin/horarios.php
+++ admin/horarios.php
@@ -318,6 +318,11 @@
                                         <a href="?action=view&id='.$horarioid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
                                        &nbsp;&nbsp;&nbsp;
                                        <a href="?action=drop&id='.$horarioid.'&name='.$titulo.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-delete"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Remove</font></button></a>
+                                       &nbsp;&nbsp;&nbsp;
+                                       
+                                       <a href="agendar.php?id='.$horarioid.'" class="non-style-link"><button  class="login-btn btn-primary-soft btn "  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Book Now</font></button></a>
+
+                                       
                                         </div>
                                         </td>
                                     </tr>';
