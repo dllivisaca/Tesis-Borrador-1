4fc0f0a17f1eb2c5791463b07931e2ceb63d2952
Daisy Llivisaca <daisynohelia@gmail.com>
2024-11-18 13:30:29 -0500

    admin: creacion de archivo enviar_recordatorio.php solo funciona con la sandbox, falta esperar que twilio y meta aprueben el número

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e60fd8335965cba6eea6dc3d469828d10a5ebbb1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e60fd8335965cba6eea6dc3d469828d10a5ebbb1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.3.8',
+            'version' => '8.3.8.0',
+            'reference' => 'd05c08d0b9ed017f61ebbc31bc4e5f4d758a0326',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
