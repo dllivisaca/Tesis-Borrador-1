b66632193f4786e357c16cfdbe8bf03f5c715713
Daisy Llivisaca <daisynohelia@gmail.com>
2024-11-27 10:19:05 -0500

    admin: doctores, modificación de diseño (parte 2)

--- admin/doctores.php
+++ admin/doctores.php
@@ -6,16 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/base.css">
-    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/doctores.css">
     <title>Doctores</title>
-    <style>
-        .popup{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-        .sub-table{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-</style>
+    
 </head>
 <body>
     <?php
@@ -63,50 +56,28 @@
                 </form>
             </div>
         </div>
-                <tr>
-                    
-                    <td>
-                        <form action="" method="post" class="header-search">
-                            
-                            
-                            <?php
-                                echo '<datalist id="doctores">';
-                                $list11 = $database->query("select docnombre,docusuario from  doctor;");
+                
+            <form action="" method="post" class="header-search">
+                
+                
+                <?php
+                    echo '<datalist id="doctores">';
+                    $list11 = $database->query("select docnombre,docusuario from  doctor;");
 
-                                for ($y=0;$y<$list11->num_rows;$y++){
-                                    $row00=$list11->fetch_assoc();
-                                    $d=$row00["docnombre"];
-                                    $c=$row00["docusuario"];
-                                    echo "<option value='$d'><br/>";
-                                    echo "<option value='$c'><br/>";
-                                };
+                    for ($y=0;$y<$list11->num_rows;$y++){
+                        $row00=$list11->fetch_assoc();
+                        $d=$row00["docnombre"];
+                        $c=$row00["docusuario"];
+                        echo "<option value='$d'><br/>";
+                        echo "<option value='$c'><br/>";
+                    };
 
-                            echo ' </datalist>';
-?>                                             
-                        </form>
-                        
-                    </td>
-                    <!-- <td width="15%">
-                        <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
-                            Today's Date
-                        </p>
-                        <p class="heading-sub12" style="padding: 0;margin: 0;">
-                            <?php 
-                            //date_default_timezone_set('America/Guayaquil');
-
-                            //$fecha = date('Y-m-d');
-                            //echo $fecha;
-    
-                        ?>
-                        </p>
-                    </td> -->
-                    <!-- <td width="10%">
-                        <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
-                    </td> -->
-                </tr>
-               
+                echo ' </datalist>';
+                ?>                                             
+            </form>
+                  
                 <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 15px; padding-right: 15px;">
-                    <p class="heading-main12" style="margin-left: 45px; font-size: 18px; color: rgb(49, 49, 49);">
+                    <p class="heading-main12" style="margin-left: 45px; font-size: 17px; color: rgb(49, 49, 49);">
                         Todos los Doctores (<?php echo $list11->num_rows; ?>)
                     </p>
                     <a href="?action=add&id=none&error=0" class="non-style-link">
@@ -131,7 +102,7 @@
                    <td colspan="4">
                        <center>
                         <div class="abc scroll">
-                        <table width="93%" class="sub-table scrolldown" border="0">
+                        <table  class="sub-table scrolldown" border="0">
                         <thead>
                         <tr>
                                 <th class="table-headin">
@@ -166,7 +137,7 @@
                                     <img src="../img/notfound.svg" width="25%">
                                     
                                     <br>
-                                    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+                                    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">No existen resultados para el texto ingresado</p>
                                     <a class="non-style-link" href="doctores.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Doctors &nbsp;</font></button>
                                     </a>
                                     </center>
@@ -219,9 +190,7 @@
                         </center>
                    </td> 
                 </tr>
-                       
-                        
-                        
+       
             </>
         </div>
     </div>
@@ -270,16 +239,12 @@
                     <center>
                         <h2></h2>
                         <a class="close" href="doctores.php">&times;</a>
-                        <div class="content">
-                            eDoc Web App<br>
-                            
-                        </div>
                         <div style="display: flex;justify-content: center;">
                         <table width="80%" class="sub-table scrolldown add-doc-form-container" border="0">
                         
                             <tr>
                                 <td>
-                                    <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">View Details.</p><br><br>
+                                    <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">Ver detalles</p><br><br>
                                 </td>
                             </tr>
                             
